8354fcdd499dc53d0f9a207c31f3f18a31d85970
lianming <15127004511@qq.com>
2021-08-23 12:15:05 +0800

    去掉参考资料

--- 1.glsl
+++
@@ -1,9 +0,0 @@
- a(void) {
-  vec2 poissonDisk[10];
-  return poissonDisk;
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-
-}
--- visible/readme.md
+++
@@ -1,1 +0,0 @@
-[](https://ed.ilogues.com/2016/02/19/interactive-spherical-harmonic-visualization)
